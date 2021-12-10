e2f467154b03fa325e3623d3e1aa5eadd21c4824
danielrodrigues2019 <danielrodrigues129@hotmail.com>
2021-12-10 20:40:42 -0300

    update eslint and jest config

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,5 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/', '/.out/', '/public/'],
 
-  setupFilesAfterEnv: [
-    './src/__setup__/setupTests.js',
-  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
 };
